068e7bfb5c2e0e4c6799c972d47854809050fcbf
sebastian gonzalez trujillo <sebastiangonza1505@hotmail.com>
2023-10-08 16:23:06 -0500

    Creacion de metodo guardar usuario

--- components/usuarios/crearUsuario.vue
+++ components/usuarios/crearUsuario.vue
@@ -15,16 +15,26 @@
                                     placeholder="Apellidos" variant="outlined" />
                             </v-col>
                             <v-col cols="12">
+                                <v-autocomplete v-model="usuarioNuevo.tipo" :rules="[rules.required]" :items="Tipos_usu"
+                                    label="Tipo de usuario" placeholder="Select..." ></v-autocomplete>
+                            </v-col>
+                            <v-col cols="12">
                                 <v-text-field v-model="usuarioNuevo.correo" :rules="[rules.required, rules.email]"
                                     label="Correo" variant="outlined" cols="6" />
                             </v-col>
                             <v-col cols="6">
-                                <v-text-field v-model="usuarioNuevo.contrasena" :rules="[rules.required]" label="Contrasena"
-                                    placeholder="Contraseña" variant="outlined" />
+                                <v-text-field v-model="usuarioNuevo.contrasena"
+                                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]"
+                                    :type="show1 ? 'text' : 'password'" hint="Minimo 8 caracteres" counter
+                                    @click:append="show1 = !show1" label="Contraseña" placeholder="Contraseña"
+                                    variant="outlined" />
                             </v-col>
                             <v-col cols="6">
-                                <v-text-field v-model="usuarioNuevo.conficontrasena" :rules="[rules.required]"
-                                    label="Comfirmar contraseña" placeholder="Comfirmar contraseña" variant="outlined" />
+                                <v-text-field v-model="usuarioNuevo.conficontrasena"
+                                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]"
+                                    :type="show1 ? 'text' : 'password'" hint="Minimo 8 caracteres" counter
+                                    @click:append="show1 = !show1" label="Confirmar contraseña"
+                                    placeholder="Confirmar contraseña" variant="outlined" />
                             </v-col>
                             <v-col cols="6">
                                 <v-btn class="text-none" color="#616161" variant="flat" type="submit" size="large" block>
@@ -35,7 +45,7 @@
                             <v-col cols="6">
                                 <v-btn border class="text-none" variant="text" @click="closeDialog()" size="large">
                                     Cancelar
-                                    <v-icon class="mx-1" end icon="mdi-cancel"/>
+                                    <v-icon class="mx-1" end icon="mdi-cancel" />
                                 </v-btn>
                             </v-col>
                         </v-row>
@@ -46,6 +56,8 @@
     </div>
 </template>
 <script setup>
+import axios from 'axios';
+
 
 const open = ref()
 
@@ -65,14 +77,20 @@ onBeforeMount(() => {
 });
 
 const guardarUsuario = () => {
-    if (usuarioNuevo.value.contrasela == usuarioNuevo.value.conficontrasena) {
-
+    if (usuarioNuevo.value.contrasena === usuarioNuevo.value.conficontrasena) {
+        console.log("Hola");
+        axios.post("http://localhost:3001/usuario", usuarioNuevo.value)
+            .then(() => {
+                closeDialog();
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    } else {
+        alert("Contraseñas incorrectas");
     }
-    else{
-        
-    }
-
 };
+
 
 const closeDialog = () => {
     open.value = false
@@ -85,13 +103,17 @@ const closeDialog = () => {
 export default {
     data() {
         return {
+            show1: false,
+            show2: true,
             email: '',
+            Tipos_usu: ['Barbero', 'Cliente'],
             rules: {
                 required: value => !!value || 'Campo necesaro.',
                 email: value => {
                     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                     return pattern.test(value) || 'Correo invalido.'
-                }
+                },
+                min: v => v.length >= 8 || 'Min 8 characters'
             }
         }
     }
